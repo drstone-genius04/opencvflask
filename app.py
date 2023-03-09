b13b1e003af1267b88a82237e7d20aae5de6f967
drstone-genius04 <82304884+drstone-genius04@users.noreply.github.com>
2023-03-09 19:44:55 +0530

    second commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask,redirect,url_for,render_template,request,Response
+import cv2
+import numpy as np
+
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+def gen_frames():  # generate frame by frame from camera
+    while True:
+        sucess,frame=camera.read()
+        if not sucess:
+            break
+        else:
+            detector=cv2.CascadeClassifier('Harrcasades/haarcascade_frontalface_default.xml')
+            eye_cascade=cv2.CascadeClassifier('Harrcasades/haarcascade_eye.xml')
+            faces=detector.detectMultiScale(frame,1.1,7)
+            gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+            for (x,y,w,h) in faces:
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+                roi_gray=gray[y:y+h,x:x+w]
+                roi_color=frame[y:y+h,x:x+w]
+                eyes=eye_cascade.detectMultiScale(roi_gray,1.1,3)
+                
+                for (ex,ey,ew,eh) in eyes:
+                    cv2.rectangle(frame,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            ret,buffer= cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+            yield (b'--frame\r\n'
+                     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/video')
+def video():
+    return Response(gen_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+if __name__ == '__main__':
+    app.run(debug=True)
